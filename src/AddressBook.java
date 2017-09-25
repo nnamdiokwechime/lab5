c2103a48a5cde401cac24611ba6cdc5f84f2d53b
nnamd <nnamd@172.17.202.220>
2017-09-25 09:35:36 -0400

    I dont know what this commit thing means

---
+++ src/AddressBook.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * @author Nnamdi Okwechime
+ *
+ */
+public class AddressBook {
+
+	Map<String,BuddyInfo> contacts;
+	
+
+	public AddressBook(){
+		contacts = new HashMap<String,BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo contact) {
+		contacts.put(contact.getName(), contact);
+				
+	}
+	
+	public void removeBuddy(String key) {
+		contacts.remove(key);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Address Book");
+
+	}
+	
+	
+	
+	
+
+}
